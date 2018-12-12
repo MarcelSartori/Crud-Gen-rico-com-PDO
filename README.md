28d24fbcae888dd88f65bbc62fce0b72854c49a7
MarcelSartori <contato@mslwebstudio.com.br>
2018-12-12 19:16:03 -0300

    Atualizações de Informações do Projeto
    Atualizações da Documentação e Exemplos de uso.

--- README.md
+++ README.md
@@ -1,6 +1,49 @@
 # CRUD genérico utilizando PDO e OOP
 Este é um pequeno exemplo de Crud genérico utilizando PDO e PHP Orientado a objetos.
 
-Exemplos de utilização podem ser vistos no arquivo exemplo.php
-
 Fique a vontade para contribuir com este projeto! =)
+Dê um Fork no projeto e colabore.
+
+
+# Exemplo de Utilização
+
+Para utilizar o CRUD genérico, basta criar sua classe declarando os métodos delete e read como métodos abstratos.
+
+CRIAÇÃO DE USUÁRIO
+// Defino as variáveis
+$nome = "Meu nome";
+$email = "contato@comtoso.com";
+
+// Gravo na minha classe
+$usuario->setNome($nome);
+$usuario->setEmail($email);
+
+// Crio o usuário
+$usuario->create();
+
+
+UPDATE DE USUÁRIO
+// Defino as variáveis
+$nome = "Meu nome";
+$email = "contato@comtoso.com";
+
+// Gravo na minha classe
+$usuario->setNome($nome);
+$usuario->setEmail($email);
+
+// Crio o usuário
+$usuario->update(1);
+
+
+LEITURA DOS DADOS DO USUÁRIO
+$id = 1;
+
+$user = $usuario->read($id);
+
+echo $user->id.'</br>';
+echo $user->nome.'</br>';
+echo $user->email.'</br><hr>';
+
+
+DELETANDO USUÁRIOS
+$usuario->delete(1);
--- exemplo.php
+++ exemplo.php
@@ -7,47 +7,6 @@ function __autoload($classe){
 // Instancio a classe
 $usuario = new Usuarios();
 
-
-### CRIAÇÃO DE USUÁRIO
-// Defino as variáveis
-$nome = "Meu nome";
-$email = "contato@comtoso.com";
-
-// Gravo na minha classe
-$usuario->setNome($nome);
-$usuario->setEmail($email);
-
-// Crio o usuário
-if ($usuario->create()) {
-	echo "Usuário criado com sucesso!";
-}
-
-
-### UPDATE DE USUÁRIO
-// Defino as variáveis
-$nome = "Meu nome";
-$email = "contato@comtoso.com";
-
-// Gravo na minha classe
-$usuario->setNome($nome);
-$usuario->setEmail($email);
-
-// Crio o usuário
-if ($usuario->update(1)) {
-	echo "Usuário atualizado com sucesso!";
-}
-
-
-### lEITURA DOS DADOS DO USUÁRIO
-$id = (int)1;
-
-$user = $usuario->read($id);
-
-echo $user->id.'</br>';
-echo $user->nome.'</br>';
-echo $user->email.'</br><hr>';
-
-
 ### LEITURA DOS DADOS DE TODOS OS USUÁRIOS
 foreach($usuario->readAll() as $key => $user):
 
@@ -56,10 +15,3 @@ foreach($usuario->readAll() as $key => $user):
 	echo $user->email.'</br><hr>';
 
 endforeach;
-
-
-
-### DELETO O USUÁRIO
-if ($usuario->delete(1)) {
-	echo "Usuário removido com sucesso!";
-}
